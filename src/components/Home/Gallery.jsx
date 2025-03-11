46a9b209128628eaa920ecfc1534203f2bd73a12
Hvostyk <natlitvinandmat@gmail.com>
2025-03-12 01:02:06 +0300

    Это просто)

--- src/components/Home/Gallery.jsx
+++ src/components/Home/Gallery.jsx
@@ -1,58 +1,53 @@
-import style from "../../styles/Home/Gallery.module.css"
+import style from "../../styles/Home/Gallery.module.css";
 import { Gallery_Images } from "../../assets/Images.jsx";
-import { Fragment } from "react";
-import { useRef } from "react";
-import { useState } from "react";
+import { Fragment, useRef, useState } from "react";
 
-const container=Gallery_Images.map((item)=>(
+const container = Gallery_Images.map((item) => (
     <Fragment key={item.id}>
-    <div id={item.id} className={style["Gallery_Images"]}>
-        <img src={item.source} alt={item.id} />
+        <div id={item.id} className={style["Gallery_Images"]}>
+            <img src={item.source} alt={item.id} />
         </div>
     </Fragment>
 ));
 
-
-function Gallery(){
-
+function Gallery() {
     const maxKey = Gallery_Images.length - 1;
     const [currentPos, setCurrentPos] = useState(0);
+    const [offset, setOffset] = useState(0);
 
-    const useImage_pos=()=>{
-        function onClick(action){
-            switch(action){
+    const useImage_pos = () => {
+        function onClick(action) {
+            switch (action) {
                 case "next":
-                    console.log("prevcon"+" "+prevcon.length)
-                    console.log("nextcon"+" "+nextcon.length)
+                    console.log(currentPos)
                     setCurrentPos((prev) => (prev + 1 > maxKey ? 0 : prev + 1));
+                    setOffset((prev) => prev - 1250);
                     break;
                 case "prev":
-                    console.log("prevcon"+" "+prevcon.length)
-                    console.log("nextcon"+" "+nextcon.length)
+                    console.log(currentPos)
                     setCurrentPos((prev) => (prev - 1 < 0 ? maxKey : prev - 1));
+                    setOffset((prev) => prev + 1250);
                     break;
                 default:
                     break;
             }
         }
-        return{
+        return {
             onClick,
-        }
-    }
+        };
+    };
+
 
     const prevcon = currentPos === 0 ? [] : container.slice(0, currentPos);
     const currentcon=container[currentPos]
     const nextcon = container.slice(currentPos + 1, maxKey + 1);
 
-    const img_pos=useImage_pos()
+    const img_pos = useImage_pos();
 
-    console.log("prevcon length:", prevcon);
-    console.log("nextcon length:", nextcon);
-    console.log(currentPos)
-    return(
+    return (
         <div className={style["Gallery"]}>
-            <div className={style["Slides"]}>
-                <ul className={style["prevCon"]}>
+            <div className={style["Slides"]} style={{ transform: `translateX(${offset}px)` }}>
+            <ul className={style["prevCon"]}>
                         {prevcon.map((item, index) => (
                             <li key={index}>{item}</li>
                         ))}
@@ -68,11 +63,10 @@ function Gallery(){
                         ))}
                     </ul>
             </div>
-            
-        <button onClick={() => img_pos.onClick("prev")}>prev</button>
-        <button onClick={() => img_pos.onClick("next")}>next</button>
+            <button className={style["PrevButt"]} onClick={() => img_pos.onClick("prev")}>prev</button>
+            <button className={style["NextButt"]} onClick={() => img_pos.onClick("next")}>next</button>
         </div>
-    )
+    );
 }
 
 export default Gallery;
